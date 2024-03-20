4a31469fc2aeec4820dc9b2ff1afa417c8478320
Sam Yates-Smith <samyatessmith@gmail.com>
2024-03-20 13:55:10 +0000

    4.1 Adjusted entire code to pass through the Code Institute PEP8Linter. After numerous adjustments, code passes.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Welcome to Riddles in the Dark.  Inspired by Bilbo and Gollum from the famous sc
 
 ## Gameplay Overview
 
-RITD combines classic riddle solving with innovative gameplay mechanics.  Players must answer x5 questions on their route to victory.  Furthermore, players must attempt to score the least amount of points possible in order to win.  The game offers the player the opportunity to take hints when they are unsure of the riddles solution.  These hints have a point penalty associated with them and therefore raise ones total score.  PLayers are also given a number of underscores ('_').  These are to advise the player how many letters the answer includes.  If a player get the riddle wrong at any point, regardless of hints taken, or if they make a mistake in the spelling of their answer (capitals not relevant) they will incur a points penalty.  Finally, if a player wants to skip a question altogether the player recieves a hefty 10 point fine to their total. 
+RITD combines classic riddle solving with innovative gameplay mechanics.  Players must answer x5 questions on their route to victory.  Furthermore, players must attempt to score the least amount of points possible in order to win.  The game offers the player the opportunity to take hints when they are unsure of the riddles solution.  These hints have a point penalty associated with them and therefore raise ones total score.  Players are also given a number of underscores ('_').  These are to advise the player how many letters the answer includes.  If a player get the riddle wrong at any point, regardless of hints taken, or if they make a mistake in the spelling of their answer (capitals not relevant) they will incur a points penalty.  Finally, if a player wants to skip a question altogether the player recieves a hefty 10 point fine to their total. 
 
 The game, in addition to the 1 player game, the game offers the opportunity for a 2 player feature.  The 2 player game follows the same principles in terms of its rulings but alternates between the players, therefore the game will ask a total of 10 questions, 5 for each player. The 2 player game mode also has methodology for settling when the game ends in a tie.  This is in the form of a "sudden death" mode, wherethe first to get an incorrect answer, loses. 
 
@@ -23,23 +23,23 @@ RITD used the following technologies:
 
 
 ## How to play
-- Game Link: https://riddlesinthedark-e09df88ee1fe.herokuapp.com/
-- Inititate Game via Heroku, follow instructions above.
-- Input your name
-- Select how many players are taking part, 1 or 2.
-- Game loads following short countdown
-- Once riddle has been asked, a selection of underscores are presented, use these as a guide for the number of letters within the expected answer, for example: _ _ _ _ could be "coin"
-- Input answer
-- Game feedback as to answers correctness
-- if answer unknown to player, type hint for a hint or skip to skip
-- Continue until 5 questions asked. 
-- Within 2 player game mode, sudden death will settle ties.  No hints are available for this gamemode so the difficulty rises exponentially. 
+1. Game Link: https://riddlesinthedark-e09df88ee1fe.herokuapp.com/
+2. Inititate Game via Heroku, follow instructions above.
+3. Input your name
+4. Select how many players are taking part, 1 or 2.
+5. Game loads following short countdown
+6. Once riddle has been asked, a selection of underscores are presented, use these as a guide for the number of letters within the expected answer, for example: _ _ _ _ could be "coin"
+7. Input answer
+8. Game feedback as to answers correctness
+9. if answer unknown to player, type hint for a hint or skip to skip
+10. Continue until 5 questions asked. 
+11. Within 2 player game mode, sudden death will settle ties.  No hints are available for this gamemode so the difficulty rises exponentially. 
 
 ## Import modules
 
-- Time
-- Textwrap
-- Random
+- Time.  The time module is required within the countdown function.  It allows for the implementation of the anticipoation created by the slight delay in the games start and the 3, 2, 1... countdown
+- Textwrap.  Textwrap is necessary to ensure the character width limit.  It allows for the function text_wrap.
+- Random.  Random allows the shuffling of querstions to prolong engagement within the game.  User would not use the game if the questions were the same everytime the game ios played.
 
 ## Features list
 
@@ -58,22 +58,25 @@ RITD used the following technologies:
 - Extensive Error prompting
 - Sudden death mode in 2 player gamemode
 
-## Preliminary considerations and planning 
+## Preliminary planning 
 
 - Flow diagram: 
  <img src="https://github.com/SamYatesSmith/RiddlesInTheDark/blob/main/assets/images/Riddle%20Game%20Procedure.png">
 
 ## Deployment
 
+- Program deployed on Heroku: https://www.heroku.com/
+- 
+
 ## Further Development Opportunities
 
 - Within 2 player ,the sudden death is unblanced.  I need to build a timer into the main game so that the player who took the longest to answer their questions owuld go first, thus recieving a lesser chance to get the 1st answer correct and subseequnetly losing the game. 
-- Design and build a GUI
-- Allow for online multiplayer
-- All time high scores leaderboard
-- Add more Riddles and hints
+- Design and build a GUI.
+- Allow for online multiplayer.
+- All time high scores leaderboard.
+- Add more Riddles and hints.
 
-## Testing 
+## Testing
 
 ## Validator Approval
 
--- run.py
+++ run.py
@@ -2,13 +2,15 @@ import time
 import random
 import textwrap
 
-def wrap_text(text, width=80):
+
+def wrap_text(text, width=79):
     """
     Wraps text to the specified width
     """
-    return '\n'.join(textwrap.wrap(text, width))
+    return "\n".join(textwrap.wrap(text, width))
 
-class RiddleGame: 
+
+class RiddleGame:
     def __init__(self, filepath=None):
         """
         Initialises the riddle game with riddles loaded from external file
@@ -20,7 +22,7 @@ class RiddleGame:
         self.current_player = 0
         self.player_mode = 1
         self.player_names = ["Player 1", "Player 2"]
-        if filepath: 
+        if filepath:
             self.load_questions(filepath)
 
     def load_questions(self, filepath):
@@ -28,9 +30,9 @@ class RiddleGame:
         Loads questions, answers and hints from a specified file.
         """
         try:
-            with open(filepath, 'r') as file:
+            with open(filepath, "r") as file:
                 lines = file.read().split("\n")
-            
+
             temp_hints = []
             for line in lines:
                 if line.startswith("Q: "):
@@ -45,23 +47,39 @@ class RiddleGame:
                     if line.strip():
                         temp_hints.append(line)
             if temp_hints:
-                    self.hints.append(temp_hints)
+                self.hints.append(temp_hints)
         except FileNotFoundError:
-            print(wrap_text("Error: The file path provided does not exist.  Please check the file path and give it another whirl."))
+            print(
+                wrap_text(
+                    "Error: The file path provided does not exist.  Please "
+                    "check the file path and give it another whirl."
+                )
+            )
 
     def choose_game_mode(self):
         """
-        Prompts the user to choose the game mode, either single player or two players. Enquire after player names.
+        Prompts the user to choose the game mode, either single player or two
+        players. Enquire after player names.
         """
         while True:
             try:
-                mode = input(wrap_text("Choose your game mode: Key '1' for Single player and '2' for 2 player. 1/2:\n"))
+                mode = input(
+                    wrap_text(
+                        "Choose your game mode: Key '1' for Single player and"
+                        "'2' for 2 player. 1/2:\n"
+                    )
+                )
                 self.player_mode = int(mode)
                 if self.player_mode not in [1, 2]:
                     raise ValueError
                 break
             except ValueError:
-                print(wrap_text("Invalid input. Please enter '1' for Single player game, or '2' for Two player game."))
+                print(
+                    wrap_text(
+                        "Invalid input. Please enter '1' for Single player"
+                        "game, or '2' for Two player game."
+                    )
+                )
 
         if self.player_mode == 1:
             self.player_names[0] = input("Enter your name:\n")
@@ -75,7 +93,9 @@ class RiddleGame:
         """
         combined = list(zip(self.questions, self.answers, self.hints))
         random.shuffle(combined)
-        self.questions, self.answers, self.hints = map(list, zip(*combined)) if combined else ([], [], [])
+        self.questions, self.answers, self.hints = (
+            map(list, zip(*combined)) if combined else ([], [], [])
+        )
 
     def get_user_input(self, prompt, valid_responses=None, lower=True):
         """
@@ -87,7 +107,8 @@ class RiddleGame:
                 user_input = user_input.lower()
             if valid_responses is None or user_input in valid_responses:
                 return user_input
-            print(f"Invalid input.  Please enter one of the {valid_responses}.")
+            print(f"Invalid input.  Please enter one of the"
+                  "{valid_responses}.")
 
     def display_intro_message(self):
         """
@@ -96,26 +117,32 @@ class RiddleGame:
 
     intro_message = (
         "Welcome to Riddles in the Dark!\n\n"
-        "Riddles in the Dark is a short riddle based game. In order to claim victory, "
-        "the player must accrue as few points as possible. Points are incremented by "
-        "answering riddles incorrectly, asking for hints, using too many hints and skipping "
-        "the current question.\n\n"
-        "Players are allowed 5 hints per question. These hints will accrue 1 point each.\n\n"
-        "However, if a player uses more than 5 hints, they are punished with an incorrect "
-        "answer's point value.\n\n"
+        "Riddles in the Dark is a short riddle based game. In order to claim "
+        "victory, the player must accrue as few points as possible. Points "
+        "are incremented by answering riddles incorrectly, asking for hints, "
+        "using too many hints and skipping the current question.\n\n"
+        "Players are allowed 5 hints per question. These hints will accrue "
+        "1 point each.\n\n"
+        "However, if a player uses more than 5 hints, they are punished with "
+        "an incorrect answer's point value.\n\n"
         "When a player answers incorrectly, they receive 2 points.\n\n"
-        "When a player skips the question, they will receive a 10 point penalty.\n\n"
-        "Players are provided with underscores which represent the number of letters in the answer.\n\n"
+        "When a player skips the question, they will receive a 10 point "
+        "penalty.\n\n"
+        "Players are provided with underscores which represent the number of "
+        "letters in the answer.\n\n"
         "Good luck, and Have fun!\n\n"
     )
-    paragraphs = intro_message.split('\n\n')
-    wrapped_intro = '\n\n'.join(textwrap.fill(paragraph, width=80) for paragraph in paragraphs)
+    paragraphs = intro_message.split("\n\n")
+    wrapped_intro = "\n\n".join(
+        textwrap.fill(paragraph, width=79) for paragraph in paragraphs
+    )
 
     print(wrapped_intro)
 
     def display_letter_count_hint(self, answer):
         """
-        Dispalys a hint for the current riddle, shows the number of letters in the answer as underscores
+        Dispalys a hint for the current riddle, shows the number of letters in
+        the answer as underscores
         """
         letter_count_hint = " ".join("_" * len(answer))
         print(f"Answer letter count: {letter_count_hint}")
@@ -126,21 +153,34 @@ class RiddleGame:
         """
         self.player_scores[self.current_player] += 1
         print(wrap_text(f"Hint: {hint}"))
-        print(wrap_text(f"Hint used. Current points: {self.player_scores[self.current_player]}."))
+        print(
+            wrap_text(
+                f"Incorrect. Try again, or use a hint. Your current "
+                f"score: {self.player_scores[self.current_player]}.\n\n"
+            )
+        )
 
     def handle_question(self, question, answer, hints):
         """
-        Handles logic for asking a question. Takes users response, provides hint and skips
+        Handles logic for asking a question. Takes users response, provides
+        hint and skips
         """
-        question_text = f"Question for {self.player_names[self.current_player]}: {question}"
+        question_text = (
+            f"Question for {self.player_names[self.current_player]}: "
+            f"{question}"
+        )
         print(wrap_text(question_text))
         self.display_letter_count_hint(answer)
         hints_given = 0
 
         while True:
-            response = self.get_user_input(f"{self.player_names[self.current_player]}, Type your answer (OR type 'hint' for a hint, 'skip' to Skip): ", lower = False)
-            
-            if response.lower() == 'hint':
+            response = self.get_user_input(
+                f"{self.player_names[self.current_player]}, Type your answer "
+                f"(OR type 'hint' for a hint, 'skip' to Skip): ",
+                lower=False,
+            )
+
+            if response.lower() == "hint":
                 if hints_given < len(hints):
                     self.offer_hint(hints[hints_given])
                     hints_given += 1
@@ -152,11 +192,21 @@ class RiddleGame:
                 break
             else:
                 self.player_scores[self.current_player] += 2
-                print(wrap_text(f"Incorrect.  Try again, or use a hint.  Your current score: {self.player_scores[self.current_player]}.\n\n"))
-                
+                print(
+                    wrap_text(
+                        f"Incorrect.  Try again, or use a hint.  Your current "
+                        f"score: {self.player_scores[self.current_player]}.\n\n"
+                    )
+                )
+
             if response.lower() == "skip":
                 self.player_scores[self.current_player] += 10
-                print(wrap_text(f"Skipped.  This costs you 10 points.  Current point total: {self.player_scores[self.current_player]}"))
+                print(
+                    wrap_text(
+                        f"Skipped.  This costs you 10 points.  Current point "
+                        f"total: {self.player_scores[self.current_player]}"
+                    )
+                )
                 break
 
     def handle_turn(self, question_index):
@@ -165,7 +215,11 @@ class RiddleGame:
         """
         if question_index < len(self.questions):
             print(f"{self.player_names[self.current_player]}'s turn: ")
-            question, answer, hints = self.questions[question_index], self.answers[question_index], self.hints[question_index]
+            question, answer, hints = (
+                self.questions[question_index],
+                self.answers[question_index],
+                self.hints[question_index],
+            )
             self.handle_question(question, answer, hints)
         else:
             print("Error: Question index out of range.")
@@ -189,15 +243,20 @@ class RiddleGame:
 
     def play_game(self):
         """
-        Starts and manages games flow, from choosing the games mode to conclusion
+        Starts and manages games flow, from choosing the games mode to
+        conclusion
         """
         self.display_intro_message()
         self.choose_game_mode()
         self.shuffle_questions()
 
-        ready_prompt = "Ok, so are you ready to play? (yes/no): " if self.player_mode == 1 else "Ok, so are you both ready to play? (yes/no): "
+        ready_prompt = (
+            "Ok, so are you ready to play? (yes/no): "
+            if self.player_mode == 1
+            else "Ok, so are you both ready to play? (yes/no): "
+        )
         ready = self.get_user_input(ready_prompt, ["yes", "no"], lower=True)
-        
+
         if ready == "no":
             print("Maybe next time?!")
             return
@@ -206,56 +265,98 @@ class RiddleGame:
 
         questions_per_player = 5
         if self.player_mode == 1:
-            # In single-player mode, simply iterate through the first 5 questions
             for i in range(questions_per_player):
                 print(f"Question {i+1} of {questions_per_player}: ")
                 self.handle_turn(i)
         else:
-            # In two-player mode, ensure unique questions for each player by correctly calculating question index
-            for i in range(questions_per_player * 2): 
-                current_question_index = i // 2 if i % 2 == 0 else (i // 2) + questions_per_player
-                print(f"{self.player_names[self.current_player]}'s turn (Question {current_question_index % questions_per_player + 1} of {questions_per_player}):")
+            for i in range(questions_per_player * 2):
+                current_question_index = (
+                    i // 2 if i % 2 == 0 else (i // 2) + questions_per_player
+                )
+                print(
+                    f"{self.player_names[self.current_player]}'s turn "
+                    f"(Question "
+                    f"{current_question_index % questions_per_player + 1} "
+                    f"of {questions_per_player}): "
+                )
                 self.handle_turn(current_question_index)
                 self.next_player()
-        
-        if self.player_mode == 2 and self.player_scores[0] == self.player_scores[1]:
+
+        if self.player_mode == 2 and self.player_scores[0] == \
+                self.player_scores[1]:
             self.sudden_death()
         else:
             self.conclude_game()
 
     def conclude_game(self):
         """
-        Wraps up the game by displaying the total scores and determining the winner
+        Wraps up the game by displaying the total scores and determining the
+        winner
         """
         if self.player_mode == 1:
             if self.player_scores[0] == 0:
-                print(wrap_text(f"Flawless Victory! Incredible effort, {self.player_names[0]}. You're a riddle hero! Gollum would stand no chance!"))
+                print(
+                    wrap_text(
+                        f"Flawless Victory! Incredible effort, "
+                        f"{self.player_names[0]}. You're a riddle hero! Gollum "
+                        f"would stand no chance!"
+                    )
+                )
             else:
-                print(f"Game Over! {self.player_names[0]}, your total points: {self.player_scores[0]}.")
+                print(
+                    f"Game Over! {self.player_names[0]}, your total points: "
+                    f"{self.player_scores[0]}."
+                )
         else:
             print("Game Over!")
-            print(wrap_text(f"{self.player_names[0]}'s total points: {self.player_scores[0]}, {self.player_names[1]}'s total points: {self.player_scores[1]}."))
+            print(
+                wrap_text(
+                    f"{self.player_names[0]}'s total points: "
+                    f"{self.player_scores[0]}, {self.player_names[1]}'s total "
+                    f"points: {self.player_scores[1]}."
+                )
+            )
             if self.player_scores[0] < self.player_scores[1]:
                 if self.player_scores[0] == 0:
-                    print(wrap_text(f"Flawless Victory! Incredible effort, {self.player_names[0]}. You're a riddle hero! Gollum would stand no chance!"))
+                    print(
+                        wrap_text(
+                            f"Flawless Victory! Incredible effort, "
+                            f"{self.player_names[0]}. You're a riddle hero! "
+                            f"Gollum would stand no chance!"
+                        )
+                    )
                 else:
                     print(f"{self.player_names[0]} wins!")
             elif self.player_scores[0] > self.player_scores[1]:
                 if self.player_scores[1] == 0:
-                    print(wrap_text(f"Flawless Victory! Incredible effort, {self.player_names[1]}. You're a riddle hero! Gollum would stand no chance!"))
+                    print(
+                        wrap_text(
+                            f"Flawless Victory! Incredible effort, "
+                            f"{self.player_names[0]}. You're a riddle hero! "
+                            f"Gollum would stand no chance!"
+                        )
+                    )
                 else:
                     print(f"{self.player_names[1]} wins!")
             else:
                 print("It's a tie!")
 
-
     def sudden_death(self):
         """
         Initiates sudden death mode following a 2 player tie
         """
-        print(wrap_text(f"It's a tie! Both players have {self.player_scores[0]} points."))
+        print(
+            wrap_text(
+                f"It's a tie! Both players have {self.player_scores[0]} points"
+            )
+        )
         print("Entering the Sudden Death round...")
-        print(wrap_text("Hints are disabled in Sudden Death.  A wrong answer means you loose.  Good luck!"))
+        print(
+            wrap_text(
+                "Hints are disabled in Sudden Death.  A wrong answer means "
+                "you loose.  Good luck!"
+            )
+        )
 
         asked_questions = 10
         sudden_death_index = asked_questions
@@ -264,21 +365,41 @@ class RiddleGame:
             for player_index in range(2):
                 self.current_player = player_index
                 print(f"\n{self.player_names[self.current_player]}'s turn: ")
-                question, answer = self.questions[sudden_death_index], self.answers[sudden_death_index]
+                question, answer = (
+                    self.questions[sudden_death_index],
+                    self.answers[sudden_death_index],
+                )
                 print("Question: ", question)
                 self.display_letter_count_hint(answer)
 
                 response = self.get_user_input("Your answer: ", lower=False)
 
                 if response.lower() != answer.lower():
-                    print(wrap_text(f"Worng answer! {self.player_names[self.current_player]} loses."))
+                    print(
+                        wrap_text(
+                            f"It's a tie! Both players have "
+                            f"{self.player_scores[0]} points."
+                        )
+                    )
                     winner = self.player_names[1 - self.current_player]
-                    print(wrap_text(f"Congratulations! {winner}, you won the game!  {winner} is the Riddle King."))
+                    print(
+                        wrap_text(
+                            f"Congratulations! {winner}, you won the game! "
+                            f"{winner} is the Riddle King."
+                        )
+                    )
                     return
 
-                sudden_death_index +=1
+                sudden_death_index += 1
                 if sudden_death_index >= len(self.questions):
-                    print(wrap_text("Wow wow wow wow.  Theres some very accomplished riddling taking place here.  I hold my hands up and gracefully admit defeat.  Congratulations, you both beat me, so you both win!"))
+                    print(
+                        wrap_text(
+                            "Wow wow wow wow.  Theres some very accomplished "
+                            "riddling taking place here.  I hold my hands up "
+                            "and gracefully admit defeat.  Congratulations, "
+                            "you both beat me, so you both win!"
+                        )
+                    )
                     return
 
             self.next_player()
@@ -289,6 +410,6 @@ class RiddleGame:
         """
         self.play_game()
 
-game = RiddleGame(filepath='riddleshintsanswers.txt')
-game.play()
 
+game = RiddleGame(filepath="riddleshintsanswers.txt")
+game.play()
